dbfb837b0865bc0afe277ae79424a1bf50af6b28
nikzad-avasam <abbas.nikzad70@gmail.com>
2024-04-12 16:48:14 +0330

    add lesson 07

---
+++ lesson-07/frontend/src/components/ArticleDetails.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container alert alert-primary">
+        <h2 class="display-4">{{ article.title }}</h2>
+        <p>{{ article.body }}</p>
+        <p>{{ article.date }}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      article: {},
+    };
+  },
+  props: {
+    id: {
+      type: [Number, String],
+      required: true,
+    },
+  },
+  methods: {
+    getArticleData() {
+      fetch(`http://localhost:5000/get/${this.id}/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          this.article = data
+          console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWW")
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getArticleData();
+  },
+};
+</script>
+
+<style>
+</style>
