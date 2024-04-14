df29e9dcd99855bd0f5243eca44fc3c51422a856
nikzad-avasam <abbas.nikzad70@gmail.com>
2024-04-14 19:38:36 +0330

    add lesson 09

---
+++ lesson-09/frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="container">
+    <NavbarMain />
+    <router-view />
+  </div>
+</template>
+<script>
+import NavbarMain from "./components/NavbarMain.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavbarMain,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.text-style {
+  font-size: 30px !important;
+  font-family: dena !important;
+  color:brown !important;
+}
+</style>
---
+++ lesson-09/frontend/src/components/ArticleDetails.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container alert alert-primary">
+    <h2 class="display-4">{{ article.title }}</h2>
+    <p>{{ article.body }}</p>
+    <p>{{ article.date }}</p>
+    <hr />
+    <button @click="deleteArticle" class="btn btn-danger">DELETE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      article: {},
+    };
+  },
+  props: {
+    id: {
+      type: [Number, String],
+      required: true,
+    },
+  },
+  methods: {
+    deleteArticle() {
+      if (confirm("are you suer ? ")) {
+        // if user click ok
+        fetch(`http://localhost:5000/delete/${this.id}/`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then(() => {
+            this.$router.push({ name: "home" });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    getArticleData() {
+      fetch(`http://localhost:5000/get/${this.id}/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          this.article = data;
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getArticleData();
+  },
+};
+</script>
+
+<style>
+</style>
